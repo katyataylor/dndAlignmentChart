2f87473cb5dc7d2e0f546915c99ceebe48b40e42
kat.taylor <sofia.katya.taylor@gmail.com>
2026-06-23 16:31:53 -0500

    Establish scoring logic

--- chartGame.js
+++ chartGame.js
@@ -1,13 +1,29 @@
 window.addEventListener(`DOMContentLoaded`, () => {
     
-    // 1.Grabs elements AFTER page loads
+    // Defines initial variable containers
+    let idScore = 0;
+    let superegoScore = 0;
+    let benefitScore = 0;
+    let harmScore = 0;
+
+    // Grabs elements AFTER page loads
     const playerDot = document.querySelector(`.dot`);
     const xSlider = document.querySelector(`.x-slider`);
     const ySlider = document.querySelector(`.y-slider`);
+    
+    // The math logic
+    let totalXPoints = idScore + superegoScore;
+    let totalYPoints = harmScore + benefitScore;
 
-    // Default visual starting pos
-    playerDot.style.left = "50%";
-    playerDot.style.top = "50%";
+    // Default points
+    let xScore = totalXPoints === 0 ? 5 : (idScore / totalXPoints) * 10;
+    let yScore = totalYPoints === 0 ? 5 : (benefitScore / totalYPoints) * 10;
+
+    // Convert default scores into padded percentages for dot starting point
+    let initialXPercent = (xScore / 10) * 98 +1;
+    let initialYPercent = (yScore / 10) * 98 +1;
+    playerDot.style.left = `${initialXPercent}%`;
+    playerDot.style.top = `${initialYPercent}%`;
 
     // Listen for X-axis changes
     xSlider.addEventListener("input", () => {
